6ef9d046ab7577408ab212629da230f686bc101a
Joyfisher <joy.v.fisher@gmail.com>
2017-08-02 22:00:34 +0100

    _.first implemented with tests

--- lowbar.js
+++ lowbar.js
@@ -1,8 +1,17 @@
-var _ = {};
+let _ = {};
 
 _.identity = function (value) {
     return value;
 };
+
+_.first = function (array, n) {
+    if (arguments.length === 1) {
+        return array.shift();
+    } else if (arguments.length === 2) {
+        return array.slice(0, n);
+    }
+};
+
 
 if (typeof module !== 'undefined') {
     module.exports = _;
--- spec/lowbar.js
+++ spec/lowbar.js
@@ -9,6 +9,7 @@ describe('_', function () {
         expect(_).to.be.an('object');
     });
 });
+
 describe('#identity', function () {
     it('is a function', function () {
         expect(_.identity).to.be.a('function');
@@ -20,8 +21,29 @@ describe('#identity', function () {
         expect(_.identity('Hello World')).to.equal('Hello World');
     });
     it('should return the original value if given an array', function () {
-        var input = [1, 2, 3, 4];
-        var result = _.identity(input);
+        let input = [1, 2, 3, 4];
+        let result = _.identity(input);
         expect(result).to.equal(input);
+    });
+});
+
+describe('#first', function () {
+    it('is a function', function () {
+        expect(_.first).to.be.a('function');
+    });
+    it('should return the first element if 1 is given as a parameter', function () {
+        expect(_.first([1, 2, 3], 1)).to.eql([1]);
+    });
+    it('should return the first 2 array items if 2 is given as a parameter', function () {
+        expect(_.first([4, 5, 6], 2)).to.eql([4, 5]);
+    });
+    it('should return the given number of array items', function () {
+        expect(_.first([7, 8, 9], 3)).to.eql([7, 8, 9]);
+    });
+    it('should return undefined if not passed an argument', function () {
+        expect(_.first()).to.eql(undefined);
+    });
+    it('should return the full array if given a number greater than the array length', function () {
+        expect(_.first([1,2,3,4], 5)).to.eql([1,2,3,4]);
     });
 });
